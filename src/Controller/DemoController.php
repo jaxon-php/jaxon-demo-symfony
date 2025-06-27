65c2e3f1fdc888e624089436ab8ace523fa4fc9e
Thierry Feuzeu <thierry.feuzeu@gmail.com>
2025-06-28 00:48:10 +0200

    Add the Calc component.

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -6,21 +6,25 @@ use Demo\Ajax\App\Test as AppTest;
 use Demo\Ajax\App\Buttons as AppButtons;
 use Demo\Ajax\Ext\Test as ExtTest;
 use Demo\Ajax\Ext\Buttons as ExtButtons;
+use Jaxon\Demo\Calc\Package as CalcPackage;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+use function jaxon;
+use function rq;
 
 class DemoController extends AbstractController
 {
     #[Route('/', name: 'jaxon.home')]
     public function __invoke()
     {
-        // Print the page
         return $this->render('demo/index.html.twig', [
             'pageTitle' => "Symfony Framework",
             'rqAppTest' => rq(AppTest::class),
             'rqAppButtons' => rq(AppButtons::class),
             'rqExtTest' => rq(ExtTest::class),
             'rqExtButtons' => rq(ExtButtons::class),
+            'calcPackage' => jaxon()->package(CalcPackage::class),
         ]);
     }
 }
